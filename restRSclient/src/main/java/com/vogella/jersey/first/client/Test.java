848085a5e5f0da3c35c3b86942f975e4fa1ea6cc
Peter MOUEZA <mouezapeter@gmail.com>
2018-01-21 23:12:22 +0100

    client OK !
    Do not forget to launch restRS = service side
    TODOs get(String)

--- restRSclient/src/main/java/com/vogella/jersey/first/client/Test.java
+++ restRSclient/src/main/java/com/vogella/jersey/first/client/Test.java
@@ -11,37 +11,60 @@ import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.client.ClientConfig;
 
+//import com.google.common.collect.ImmutableBiMap.Builder;
+
+import javax.ws.rs.client.Invocation.Builder;
+
+/** http://www.vogella.com/tutorials/REST/article.html */
 public class Test {
 
-    public static void main(String[] args) {
-        ClientConfig config = new ClientConfig();
+	public static void main(String[] args) {
+		ClientConfig config = new ClientConfig();
 
-        Client client = ClientBuilder.newClient(config);
+		Client client = ClientBuilder.newClient(config);
 
-        WebTarget target = client.target(getBaseURI());
+		WebTarget target = client.target(getBaseURI());
 
-        String response = target.path("rest").
-                            path("hello").
-                            request().
-                            accept(MediaType.TEXT_PLAIN).
-                            get(Response.class)
-                            .toString();
+		/** http://localhost:9025/restRS/rest/hello */
+		// String response =
+		// target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(Response.class).toString();
 
+		// String plainAnswer =
+		// target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+		WebTarget a = target.path("rest");
+		WebTarget b = a.path("hello");
+		Builder c = b.request();
+		Builder d = c.accept(MediaType.TEXT_PLAIN);
+		String response = d.get(Response.class).toString();
 
-        String plainAnswer =
-                target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-        String xmlAnswer =
-                target.path("rest").path("hello").request().accept(MediaType.TEXT_XML).get(String.class);
-        String htmlAnswer=
-                target.path("rest").path("hello").request().accept(MediaType.TEXT_HTML).get(String.class);
+		// String plainAnswer =
+		// target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+		WebTarget a2 = target.path("rest");
+		WebTarget b2 = a2.path("hello");
+		Builder c2 = b2.request();
+		Builder d2 = c2.accept(MediaType.TEXT_PLAIN);
+		// String plainAnswer = d2.get(String.class); //TODO par String
+		String plainAnswer = d2.get(Response.class).toString();
 
-        System.out.println(response);
-        System.out.println(plainAnswer);
-        System.out.println(xmlAnswer);
-        System.out.println(htmlAnswer);
-    }
+		// String xmlAnswer =
+		// target.path("rest").path("hello").request().accept(MediaType.TEXT_XML).get(String.class);//TODO
+		String xmlAnswer = target.path("rest").path("hello").request().accept(MediaType.TEXT_XML).get(Response.class)
+				.toString();
 
-    private static URI getBaseURI() {
-        return UriBuilder.fromUri("http://localhost:8080/com.vogella.jersey.first").build();
-    }
+		// String htmlAnswer =
+		// target.path("rest").path("hello").request().accept(MediaType.TEXT_HTML).get(String.class);//TODO
+		String htmlAnswer = target.path("rest").path("hello").request().accept(MediaType.TEXT_HTML).get(Response.class)
+				.toString();
+
+		System.out.println(response);
+		System.out.println(plainAnswer);
+		System.out.println(xmlAnswer);
+		System.out.println(htmlAnswer);
+	}
+
+	private static URI getBaseURI() {
+		// return
+		// UriBuilder.fromUri("http://localhost:8080/com.vogella.jersey.first").build();
+		return UriBuilder.fromUri("http://localhost:9025/restRS").build();
+	}
 }
